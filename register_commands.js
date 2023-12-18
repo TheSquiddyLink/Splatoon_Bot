4e870ca6c22134eeadb6bee0510ec14ff3bf6570
TheSquiddyLink <preston@happychaps.com>
2023-12-18 02:05:47 -0700

    Register Commands For loop
    Updated the register commands script to use a for loop for all servers

--- register_commands.js
+++ register_commands.js
@@ -6,21 +6,24 @@ const { REST, Routes } = require('discord.js')
 
 const rest = new REST({ version: '10' }).setToken(config.discord.token);
 (async () => {
-  try {
-    console.log(commands)
-    console.log('Registering slash commands...');
-
-    await rest.put(
-      Routes.applicationGuildCommands(
-        config.discord.bot_id,
-        config.discord.server[0].id
-      ),
-      { body: commands }
-    );
-
-    console.log('Slash commands were registered successfully!');
-  } catch (error) {
-    console.log(`There was an error: ${error}`);
+  for(i in config.discord.server){
+    try {
+      console.log(commands)
+      console.log('Registering slash commands...');
+  
+      await rest.put(
+        Routes.applicationGuildCommands(
+          config.discord.bot_id,
+          config.discord.server[i].id
+        ),
+        { body: commands }
+      );
+  
+      console.log(`Slash commands were registered successfully for server ${config.discord.server[i].name}`);
+    } catch (error) {
+      console.log(`There was an error for ${config.discord.server[i].name}: ${error}`);
+    }
   }
+
 })();
 
