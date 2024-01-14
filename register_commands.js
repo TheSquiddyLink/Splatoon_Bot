dab4fcfa597e8fe303c0fb2b235e3447852e6601
TheSquiddyLink <preston@happychaps.com>
2024-01-13 19:05:53 -0700

    Register Commands Fix
    When registering commands it did not go though all servers listed

--- register_commands.js
+++ register_commands.js
@@ -6,21 +6,24 @@ const { REST, Routes } = require('discord.js')
 
 const rest = new REST({ version: '10' }).setToken(config.discord.token);
 (async () => {
-  try {
-    console.log(commands)
-    console.log('Registering slash commands...');
-
-    await rest.put(
-      Routes.applicationGuildCommands(
-        config.discord.bot_id,
-        config.discord.server[0].id
-      ),
-      { body: commands }
-    );
-
-    console.log('Slash commands were registered successfully!');
-  } catch (error) {
-    console.log(`There was an error: ${error}`);
+  for(const server of config.discord.server){
+    try {
+      console.log(commands)
+      console.log(`Registering slash commands for ${server.name}`);
+  
+      await rest.put(
+        Routes.applicationGuildCommands(
+          config.discord.bot_id,
+          server.id
+        ),
+        { body: commands }
+      );
+  
+      console.log('Slash commands were registered successfully!');
+    } catch (error) {
+      console.log(`There was an error: ${error}`);
+    }
   }
+  
 })();
 
