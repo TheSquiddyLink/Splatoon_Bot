2661b3006f1aa5a39b730001167017c097b25a4b
TheSquiddyLink <preston@happychaps.com>
2024-01-18 18:31:46 -0700

    Fix for rotation
    Some modes would not show because of deleted code, this has been fixed

--- splatoon3api.js
+++ splatoon3api.js
@@ -1,21 +1,23 @@
 const splatoon3api = require("splatoon3api");
 const Splatoon3 = new splatoon3api.Client("en-GB");
 
-function all_data(mode){
+function all_data(mode, session = 0){
     return new Promise(resolve => {
         if(mode === "salmon") {
             Splatoon3.getSalmonRun(res => {
                 console.log(res)
-                resolve(res.regularSchedules[0])
+                resolve(res.regularSchedules[session])
             })   
         } else {
             Splatoon3.getStages(res => {
                 console.log(res)
                 console.log(["open", "series"].includes(mode))
                 if(["open", "series"].includes(mode)){
-                    console.log(res.ranked[0][mode])
-                    resolve(res.ranked[0][mode])
-                } 
+                    console.log(res.ranked[session][mode])
+                    resolve(res.ranked[session][mode])
+                } else {
+                    resolve(res.regular[session])
+                }
                 
             });
         }
